269a61bb6ae137d6c69ca968387d711211c710d5
Fayroz <hndrfay45@gmail.com>
2022-08-18 15:07:19 +0300

    cw7 /2

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-function BMI (weight,height){
+function BMI(weight,height) {
 
 let result = weight / (height * height);
-return result
+return result;
 
 }
 
-console.log(BMI(10,10))
+console.log(BMI(70,1.7));
 
-function Status (bmi){
+function Status(bmi) {
 
-if ( bmi < 18.5 ){
+if (bmi < 18.5) {
 
-    return "لديك نقص في الوزن"
+    return "لديك نقص في الوزن";
+    
 }
-else if ( bmi >= 18.5 && bmi < 25 ){
+else if (bmi >= 18.5 && bmi < 25) {
 
-    return "وزنك صحي"
+    return "وزنك صحي";
 }
-else if ( bmi >= 25 ){
-    return "لديك زيادة في الوزن"
-}
-
+else if (bmi >= 25) {
+    return "لديك زيادة في الوزن";
 }
 
-function calculate (){
+}
 
-    let weight = document.getElementById(weight).value
-    let height = document.getElementById(height).value
-    let bmi = BMI(weight,height)
-    let desc = Status(bmi)
+function calculate() {
 
-    let div = document.getElementById('result')
-    div.innerTEXT = bmi + " == " + desc
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi = BMI(weight,height);
+
+    let desc = Status(bmi);
+
+    let div = document.getElementById('result');
+
+    div.innerText = bmi + " == " + desc;
 }
